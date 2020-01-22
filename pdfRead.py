3f557590be2d30c1767afe13c52b1d4cee576c17
leo <34465410+DrAtomic@users.noreply.github.com>
2020-01-21 18:54:38 -0800

    Update pdfRead.py

--- pdfRead.py
+++ pdfRead.py
@@ -5,58 +5,60 @@ import sys
 import cv2
 import pytesseract
 
+
 pdf = open(str(sys.argv[1]) , 'rb')
 reader = PyPDF2.PdfFileReader(pdf)
 
 pages = reader.getNumPages()
 
-for i in range(1,pages-1):
+
+
+for i in range(pages):
     page = reader.getPage(i)
     temp = page.extractText()
     with open("sample.txt", "a") as file_object:
         file_object.write(temp)
-    xObject = page['/Resources']['/XObject'].getObject()
-
-    for obj in xObject:
-        if xObject[obj]['/Subtype'] == '/Image':
-            size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
-            data = xObject[obj].getData()
-            if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
-                mode = "RGB"
-            else:
-                mode = "P"
-            if '/Filter' in xObject[obj]:
-                if xObject[obj]['/Filter'] == '/FlateDecode':
-                    img = Image.frombytes(mode,size,data)
-                    img.save(obj[1:] + ".png")
-                elif xObject[obj]['/Filter'] == '/DCTDecode':
-                    img = open(obj[1:] + ".jpg", "wb")
-                    img.write(data)
-                    img.close()
-                elif xObject[obj]['/Filter'] == '/JPXDecode':
-                    img = open(obj[1:] + ".jp2", "wb")
-                    img.write(data)
-                    img.close()
-                elif xObject[obj]['/Filter'] == '/CCITTFaxDecode':
-                    img = open(obj[1:] + ".tiff", "wb")
-                    img.write(data)
-                    img.close()
+    if '/XObject' in page['/Resources'].keys():
+        xObject = page['/Resources']['/XObject'].getObject()
+    
+        for obj in xObject:
+            if xObject[obj]['/Subtype'] == '/Image':
+                size = (xObject[obj]['/Width'], xObject[obj]['/Height'])
+                data = xObject[obj].getData()
+                if xObject[obj]['/ColorSpace'] == '/DeviceRGB':
+                    mode = "RGB"
                 else:
-                    img = Image.frombytes(mode, size, data)
-                    img.save(obj[1:] + ".png")
-            else:
-                print("No image found.")
+                    mode = "P"
+                if '/Filter' in xObject[obj]:
+                    if xObject[obj]['/Filter'] == '/FlateDecode':
+                        img = Image.frombytes(mode,size,data)
+                        img.save(obj[1:] + ".png")
+                        imagetoText(obj[1:] +".png")
+                    elif xObject[obj]['/Filter'] == '/DCTDecode':
+                        img = open(obj[1:] + ".jpg", "wb")
+                        img.write(data)
+                        img.close()
+                        imagetoText(obj[1:] +".jpg")
+                    elif xObject[obj]['/Filter'] == '/JPXDecode':
+                        img = open(obj[1:] + ".jp2", "wb")
+                        img.write(data)
+                        img.close()
+                        imagetoText(obj[1:] +".jp2")
+                    elif xObject[obj]['/Filter'] == '/CCITTFaxDecode':
+                        img = open(obj[1:] + ".tiff", "wb")
+                        img.write(data)
+                        img.close()
+                        imagetoText(obj[1:] +".tiff")
+                    else:
+                        img = Image.frombytes(mode, size, data)
+                        img.save(obj[1:] + ".png")
+                        imagetoText(obj[1:] +".png")
+                else:
+                    print("No image found.")
+                
 
-
-folder = os.getcwd()
-element = os.listdir(folder)
-
-
-for i in element:
-    img = cv2.imread(os.path.join(folder,i))
-    if img is not None:
-        text = pytesseract.image_to_string(img)
-        text_file = open(str(i)+'.m','w')
-        n = text_file.write(text)
-        text_file.close()
-
+def imagetoText(img):
+    text = pytesseract.image_to_string(img)
+    text_file = open(str(img) +'.txt','w')
+    n = text_file.write(text)
+    text_file.close()
